6cbce420ca99f45ea890e00709ec916afd1bb958
Vitor Felicio <desenvolvedorvitor67@gmail.com>
2026-05-30 17:01:55 -0300

    Estrutura inicial

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import { Inter_Tight } from "next/font/google"
+import type { ReactNode } from "react"
+import type { Metadata } from "next"
+import "@/app/globals.css"
+
+const inter = Inter_Tight({
+  variable: "--font-inter",
+  subsets: ["latin-ext"]
+})
+
+export const metadata: Metadata = {
+  title: "Vault",
+  description: "Armazene suas senhas com segurança"
+}
+
+interface RootLayoutProps {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="pt" className={`${inter.variable}`}>
+      <body>{children}</body>
+    </html>
+  )
+}
